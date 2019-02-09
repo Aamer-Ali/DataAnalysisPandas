bfa11f221534197fecf1403cbe8cc6cae1311530
ali <sayyed.aamer.ali@gmail.com>
2019-02-09 14:14:42 +0530

    Initial Commit

---
+++ SlicingandIndexing.py
@@ -0,0 +1,9 @@
+import pandas
+dataFrame = pandas.read_json("/home/ali/Documents/myFile.json")
+print(dataFrame)
+
+
+#Slicing
+#loc -- range inthe form of label
+#iloc -- range in the form of index number
+# ix -- range mixed labels and index
